143a0befbc56343ee10ab52771b4373e3c88101a
Ranarch988 <4pgtz9kvmj@privaterelay.appleid.com>
2026-08-02 00:36:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+const notification = document.getElementById("notification");
+const button = document.getElementById("openBtn");
+const letter = document.getElementById("letter");
+const overlay = document.getElementById("overlay");
+
+// 2 saniye sonra bildirimi göster
+setTimeout(() => {
+  notification.classList.add("show");
+}, 2000);
+
+// Butona basınca
+button.onclick = () => {
+
+  notification.style.display = "none";
+
+  overlay.style.display = "block";
+
+  letter.style.display = "block";
+
+  setTimeout(() => {
+    letter.classList.add("show");
+  }, 50);
+
+};
